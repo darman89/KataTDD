e28cc36be9b8bb59d702711aa509b9fe981b5a51
Armando Acosta Guerrero <darman.89@gmail.com>
2019-09-29 16:33:40 -0500

    - Iteración 1, Paso 5: se agrega el método test_values_two_numbers() para probar el string con dos números (Rojo)

--- StatisticsTest.py
+++ StatisticsTest.py
@@ -9,3 +9,8 @@ class StatisticsTest(TestCase):
 
     def test_values_one_number(self):
         self.assertEqual(Statistics().values("1"), [1], "Un Número")
+        self.assertEqual(Statistics().values("2"), [2], "Un Número")
+        self.assertEqual(Statistics().values("3"), [3], "Un Número")
+
+    def test_values_two_numbers(self):
+        self.assertEqual(Statistics().values("1,2"), [1, 2], "Dos Números")
