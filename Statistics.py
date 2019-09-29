bed3c78d7541a12fb9dbd11c9ab452fc300d4479
Armando Acosta Guerrero <darman.89@gmail.com>
2019-09-29 16:42:39 -0500

    - Iteración 1, Paso 6: se modifica el método values() para que pase la prueba para un string con dos números (Verde)

--- Statistics.py
+++ Statistics.py
@@ -2,5 +2,7 @@ class Statistics:
     def values(self, string):
         if string == "":
             return []
+        elif ',' in string:
+            return [int(string[0]), int(string[2])]
         else:
             return [int(string)]
