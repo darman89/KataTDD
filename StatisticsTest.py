5daa5d8b4d9839720e097b0b7d909acf7be4bede
Armando Acosta Guerrero <darman.89@gmail.com>
2019-09-29 14:57:30 -0500

    Iteración 1, Paso 3: se agrega el método test_values_one_number() para probar con un número (Rojo)

--- StatisticsTest.py
+++ StatisticsTest.py
@@ -6,3 +6,6 @@ from Statistics import Statistics
 class StatisticsTest(TestCase):
     def test_values(self):
         self.assertEqual(Statistics().values(""), [], "Cadena Vacía")
+
+    def test_values_one_number(self):
+        self.assertEqual(Statistics().values("1"), [1], "Un Número")
